9a9ae08e87d088abb8d09ae0e166cd62fbd73c79
rainfiel <rainfiel@gmail.com>
2016-07-06 16:24:04 +0800

    EJOY_API

--- lib/screen.h
+++ lib/screen.h
@@ -1,12 +1,13 @@
 #ifndef ejoy_2d_screen_h
 #define ejoy_2d_screen_h
 #include <stdbool.h>
+#include "ejoy2d.h"
 
 struct render;
 struct vertex_pack;
 
 void screen_initrender(struct render *R);
-void screen_init(float w, float h, float scale);
+EJOY_API void screen_init(float w, float h, float scale);
 void screen_trans(float *x, float *y);
 void screen_scissor(int x, int y, int w, int h);
 bool screen_is_visible(float x,float y);
